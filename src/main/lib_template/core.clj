7c4d9f2b18bd827ad764bcf194fab3a17814a31c
cark <carkhy@gmail.com>
2019-07-29 08:32:56 +0200

    first commit

---
+++ src/main/lib_template/core.clj
@@ -0,0 +1,8 @@
+(ns lib-template.core)
+
+(defn my-function
+  "This function launches its parameters to the moon *and back*.
+
+While this is no small feat, the doc it produces on cljdocs is even more striking."
+  [param]
+  param)
