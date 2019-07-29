98e4acff67ab40b0c6d2896401fe9332af13333f
cark <carkhy@gmail.com>
2019-07-29 10:35:56 +0200

    Bold text in docs !

--- src/main/lib_template/core.clj
+++ src/main/lib_template/core.clj
@@ -1,8 +1,8 @@
 (ns lib-template.core)
 
 (defn my-function
-  "This function launches its parameters to the moon *and back*.
+  "This function launches its parameters to the moon **and back**.
 
-While this is no small feat, the doc it produces on cljdocs is even more striking."
+While this is no small feat, the doc it produces on cljdocs are even more striking."
   [param]
   param)
